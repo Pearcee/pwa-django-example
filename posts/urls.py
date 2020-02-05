9c6527053cea30a871f36883c7a1111e4bc04989
Steven Pearce <steven@waverley-housing.co.uk>
2020-02-05 15:07:23 +0000

    02/05/2020 15:07:22

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('base_layout', views.base_layout, name='base_layout'),
+    path(r'getdata',views.getdata,name='getdata')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,19 +1,31 @@
 from django.shortcuts import render
 from django.core import serializers
+from django.http import HttpResponse
 from . models import feed
 import json
 # Create your views here.
 
 
-def index(request):
-    template = 'posts/index.html'
-    results = feed.objects.all()
-    context = {
-        'results': results,
-    }
-    return render(request, template, context)
-
-
 def base_layout(request):
     template = 'posts/base.html'
     return render(request, template)
+
+    
+def getdata(request):
+	results=feed.objects.all()
+	jsondata = serializers.serialize('json',results)
+	return HttpResponse(jsondata)
+
+
+def index(request):
+	template='posts/index.html'
+	results=feed.objects.all()
+	jsondata = serializers.serialize('json',results)
+	context={
+		'results':results,
+		'jsondata':jsondata,
+	}
+	return render(request,template,context)
+
+
+
