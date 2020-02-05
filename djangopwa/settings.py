e4ff8c2594334008deb6008c398e35b63f614930
Steven Pearce <steven@waverley-housing.co.uk>
2020-02-05 10:50:53 +0000

    02/05/2020 10:50:52

--- djangopwa/settings.py
+++ djangopwa/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
 PWA_APP_NAME = 'My App' 
 PWA_APP_DESCRIPTION = "My app description" 
