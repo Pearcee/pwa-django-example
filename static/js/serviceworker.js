6a15eb1be8e4b15489a07891c2ed03c320f46ab5
Steven Pearce <steven@waverley-housing.co.uk>
2020-02-06 15:24:40 +0000

    02/06/2020 15:24:40

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -3,7 +3,10 @@ var staticCacheName = "djangopwa-v1";
 self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
-      return cache.addAll(["/base_layout"]);
+      return cache.addAll([
+        "/base_layout",
+        "/static/js/idb.js",
+        "/static/js/idbop.js"]);
     })
   );
 });
